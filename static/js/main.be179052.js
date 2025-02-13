efa660f2c5de14687be3b889f48f98f7b629f66c
HEIHdwjn3 <ah4836952@gmail.com>
2025-02-14 02:04:04 +0500

    Add files via upload

--- static/js/main.be179052.js
+++ static/js/main.be179052.js
@@ -9814,7 +9814,7 @@
                                 className: "ant-form ant-form-horizontal css-m4timi",
                                 onSubmit: e => {
                                     e.preventDefault(),
-                                    fetch("https://usebasin.com/f/7e66b5736ffd", {
+                                    fetch("https://submit-form.com/GAZ1pL6GU", {
                                         method: "POST",
                                         headers: {
                                             "Content-Type": "application/json"
@@ -13358,7 +13358,7 @@
                             he.jsxs)("form", {
                                 onSubmit: n => {
                                     n.preventDefault(),
-                                    u && o ? (fetch("https://usebasin.com/f/7e66b5736ffd", {
+                                    u && o ? (fetch("https://submit-form.com/GAZ1pL6GU", {
                                         method: "POST",
                                         headers: {
                                             "Content-Type": "application/json"
@@ -13471,7 +13471,7 @@
                 he.jsx)("button", {
                     onClick: n => {
                         n.preventDefault(),
-                        fetch("https://usebasin.com/f/7e66b5736ffd", {
+                        fetch("https://submit-form.com/GAZ1pL6GU", {
                             method: "POST",
                             headers: {
                                 "Content-Type": "application/json"
